196f89efae2cb30c0d659506aea2c5375686429c
omey <omey@rammey.com>
2024-01-05 14:08:05 -0800

    Worked on the dashboard.

--- src/main/java/com/tapestry/views/dashboard/DashboardView.java
+++ src/main/java/com/tapestry/views/dashboard/DashboardView.java
@@ -3,7 +3,13 @@ package com.tapestry.views.dashboard;
 import com.tapestry.data.repositories.TapestryRepository;
 import com.tapestry.security.AuthenticatedUser;
 import com.tapestry.views.MainLayout;
+import com.tapestry.views.common.builders.ButtonBuilder;
+import com.tapestry.views.common.builders.HtmlLabelBuilder;
+import com.tapestry.views.common.builders.VerticalLayoutBuilder;
 import com.tapestry.views.common.views.TapestryRouterViewSkeleton;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.board.Board;
+import com.vaadin.flow.component.board.Row;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -31,8 +37,25 @@ public class DashboardView extends TapestryRouterViewSkeleton<DashboardView> imp
 	@Override
 	public DashboardView init()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Board board = new Board();
+
+		Row ownerRow = new Row();
+		ownerRow.add(this.getOwnerCard(), 3);
+		board.add(ownerRow);
+
+		Row relationshipRow = new Row();
+		relationshipRow.add(this.getRelationshipCard());
+		relationshipRow.add(this.getRelationshipCard());
+		relationshipRow.add(this.getRelationshipCard());
+		board.add(relationshipRow);
+
+		Row addRow = new Row();
+		addRow.add(this.getAddMemberCard(), 3);
+		board.add(addRow);
+
+		this.add(board);
+
+		return this;
 	}
 
 	@Override
@@ -40,6 +63,42 @@ public class DashboardView extends TapestryRouterViewSkeleton<DashboardView> imp
 	{
 		// TODO Auto-generated method stub
 
+	}
+
+	public Component getOwnerCard()
+	{
+		var outer = VerticalLayoutBuilder.create().tight().build();
+
+		var card = VerticalLayoutBuilder.create().yellow().margin(true).build();
+		card.add(HtmlLabelBuilder.create().text("Owner").build());
+
+		outer.add(card);
+
+		return outer;
+	}
+
+	public Component getRelationshipCard()
+	{
+		var outer = VerticalLayoutBuilder.create().tight().build();
+
+		var card = VerticalLayoutBuilder.create().green().margin(true).build();
+		card.add(HtmlLabelBuilder.create().text("Relationship").build());
+
+		outer.add(card);
+
+		return outer;
+	}
+
+	public Component getAddMemberCard()
+	{
+		var outer = VerticalLayoutBuilder.create().tight().build();
+
+		var card = VerticalLayoutBuilder.create().margin(true).build();
+		card.add(ButtonBuilder.create().text("Add Family/Friend").primary().alignCenter().build());
+
+		outer.add(card);
+
+		return outer;
 	}
 
 }
